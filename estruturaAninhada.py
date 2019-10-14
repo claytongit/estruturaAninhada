fce366b1f7146854d902b3a29bcd9e32ff65fecf
claytongit <54451974+claytongit@users.noreply.github.com>
2019-10-14 13:53:23 -0300

    Update estruturaAninhada.py

--- estruturaAninhada.py
+++ estruturaAninhada.py
@@ -1,7 +1,3 @@
-#Programa que le a velocidade de um veiculo e informa se ele vai ser multato ou nao.
-#O limite da rodovia é de 80km/h.
-#O valor da multa é de 7,50 por cada km a cima do limite.
-
 km = float(input('Informe a velocidade do carro em km/h: '))
 multa = (km-80) * 7.50
 if km <= 80:
